fdd1e08dc4188a986a203615888166abf98cae27
Joel Rahman <joel@flowmatters.com.au>
2023-12-06 10:08:20 +1100

    fixed geometry check on add_table

--- hydrograph/general.py
+++ hydrograph/general.py
@@ -319,7 +319,7 @@ class HydrographDataset(object):
 
   def add_table(self,table,csv_options={},fn=None,**tags):
     self.require_writable()
-    if table.geometry is not None:
+    if hasattr(table, 'geometry'):
       raise Warning('Use add_coverage to add GeoDataFrames')
     return self._add_tabular(table,'table','tables',csv_options,fn,**tags)
 
